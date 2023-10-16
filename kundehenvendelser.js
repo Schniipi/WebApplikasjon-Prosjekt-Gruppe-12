44e2eb86c5f279ae2ad57c2a6d45715308d1d935
kristintintin <112714368+kristintintin@users.noreply.github.com>
2023-10-17 00:05:36 +0200

    Update kundehenvendelser.js

--- kundehenvendelser.js
+++ kundehenvendelser.js
@@ -1,13 +1,1 @@
-function submitForm() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-  
-    var responseDiv = document.getElementById('response');
-  
-    // You can add additional logic here to handle the form, such as AJAX requests to send data to the server.
-  
-    // Simple response message for demonstration purposes:
-    responseDiv.innerHTML = `<p>Takk, ${name}! Vi har mottatt din henvendelse.</p>`;
-  }
-  
+
