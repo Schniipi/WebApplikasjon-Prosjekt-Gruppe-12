3147c60c152f8771fe8a106412882b3eebe3b763
Jens Markus Spångberg <jensspaang@gmail.com>
2023-10-17 21:33:41 +0200

    added new service

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult New_service()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
