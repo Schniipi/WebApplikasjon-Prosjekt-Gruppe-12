afaa5273aa81c195973230a0930e4d35dcd81083
Erik Nygård <erikn@uia.no>
2023-10-17 18:48:19 +0200

    navbar farge og fjernet fra login, begynnelse på pågående service

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -33,6 +33,10 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult PagaendeS() {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
