d2510713067730ed1fa18443ba103d4653c6ab30
Oliver Aaron Berg Johnston <oliveraaron03@gmail.com>
2023-10-25 15:14:04 +0200

    La til CSP som kommentar pga problemer med den og funkjsonen av programmet.

--- WebApplication1/Models/Filters/AddXssProtection.cs
+++ WebApplication1/Models/Filters/AddXssProtection.cs
@@ -14,9 +14,10 @@
 
             // Denne headeren spesifiserer en policy for å kontrollere hvilke ressurser som kan lastes av nettleseren,
             // og bidrar til å redusere potensielle XSS-angrep.
-            // default-src vil bli implementert på et senere tidspunkt ettersom gruppen fant ut denne forårsaket flere errorer...
-            // ...som gjorde at css styles og layout.cs innhold ikke ble vist for noen av gruppemedlemmene 
-            response.Headers.Add("Content-Security-Policy", /*"default-src*/ ",'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval';");
+            // Her skulle vi få inn CSP, men vi støttet på problemer der halvparten av gruppen...
+            // ...ikke fikk fremvist det som ble skrevet i css og innholdet i layout.cs på grunn av denne headeren...
+            // ...Derfor velger vi å ha denne som kommentar for nå, og videre finne ut av hvordan å løse dette problemet. 
+            // response.Headers.Add("Content-Security-Policy","default-src,'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval';");
 
             // Denne headeren spesifiserer om en nettside kan vises i en iframe, noe som hjelper med å forhindre Clickjacking-angrep.
             response.Headers.Add("X-Frame-Options", "DENY");
