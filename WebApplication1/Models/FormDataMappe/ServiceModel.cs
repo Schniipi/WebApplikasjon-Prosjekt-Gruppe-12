0d38d6c7c6f9c1324f7c126b36e7937a1c9631d2
Oliver Aaron Berg Johnston <oliveraaron03@gmail.com>
2023-11-02 17:08:57 +0100

    La tilbake FormDataMappe nok en gang

---
+++ WebApplication1/Models/FormDataMappe/ServiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models.FormDataMappe.ServiceModel
+
+
+{
+
+    public class FormData
+    {
+        [Required]
+        public string Navn { get; set; }
+        public int TelefonNummer { get; set; }
+        public string Kommentar { get; set; }
+
+    }
+
+
+}
