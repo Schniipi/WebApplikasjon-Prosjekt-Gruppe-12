d89cdcee855664bb95b7b527001620e45bc8482e
tastebutter <josefbu@uia.no>
2023-11-09 16:29:09 +0100

    Gjort slik at det nå er mulig å legge til en ny kunde, og slette en basert på kunden sin ID. Alt dette gjøres fra “New Service” siden klarte ikke å implementere slettefunskjon på “Kommende Service” siden

--- WebApplication1/Models/FormDataMappe/ServiceModel.cs
+++ WebApplication1/Models/FormDataMappe/ServiceModel.cs
@@ -1,14 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
-
 namespace WebApplication1.Models.FormDataMappe
 {
     public class FormData
     {
-        [Required]
+        public int id { get; set; }
         public string Navn { get; set; }
         public int TelefonNummer { get; set; }
         public string Kommentar { get; set; }
+
+        public int DeleteID { get; set; }
 
     }
 
