b4adfe95b8a475955710adba25ba5c278915c4a5
JosefBurkan <112654299+JosefBurkan@users.noreply.github.com>
2023-11-26 16:46:55 +0100

    oppdaterte sqlscript

--- WebApplication1/lagDatabase.sql
+++ WebApplication1/lagDatabase.sql
@@ -1,12 +1,12 @@
-﻿create database if not EXISTS NostedDB_12;
-use webAppDatabase;
+create database if not EXISTS NostedDB_12;
+use NostedDB_12;
 
 create table if not EXISTS Bruker
 (
 BrukerID INT AUTO_INCREMENT PRIMARY KEY,
 Rolle VARCHAR(20) NOT NULL,
 Brukernavn VARCHAR(20) NOT NULL,
-Passord VARCHAR(75) NOT NULL
+Passord VARCHAR(255) NOT NULL
 );
 
 create table if not EXISTS Kunde
@@ -24,18 +24,18 @@ CREATE TABLE if not EXISTS Service
 ServiceID INT AUTO_INCREMENT PRIMARY KEY,
 KundeID INT NOT NULL,
 FOREIGN KEY (KundeID) REFERENCES KUNDE(KundeID),
-ServiceDeleted BOOL NOT NULL,
 ServiceBeskrivelse VARCHAR(450) NOT NULL,
-DatoOpprettet DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+ServiceGjennomFortTid DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE if not EXISTS ServiceSkjema
 (
 ServiceID INT AUTO_INCREMENT PRIMARY KEY,
 FOREIGN KEY (ServiceID) REFERENCES Service(ServiceID),
+DatoSistEndret DATETIME DEFAULT CURRENT_TIMESTAMP,
 ServiceStatus VARCHAR(30) NOT NULL,
 ServiceKommentar VARCHAR(300) NOT NULL
-ServiceGjennomFortTid DATETIME DEFAULT CURRENT_TIMESTAMP;
 );
 
 CREATE TABLE if not EXISTS ServiceData
@@ -43,9 +43,9 @@ CREATE TABLE if not EXISTS ServiceData
 ServiceDataID INT AUTO_INCREMENT PRIMARY KEY,
 ServiceID INT,
 FOREIGN KEY (ServiceID) REFERENCES ServiceSkjema(ServiceID),
-Sjekkpunkter VARCHAR(70) NOT NULL,
-SjekkpunktType VARCHAR(20) NOT NULL,
-SjekkpunktSvar VARCHAR(20) NOT NULL
+Sjekkpunkter VARCHAR(100) NOT NULL,
+SjekkpunktType VARCHAR(50) NOT NULL,
+SjekkpunktSvar VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE if not EXISTS ServiceOgBruker
@@ -56,3 +56,5 @@ PRIMARY KEY(BrukerID, ServiceID),
 FOREIGN KEY (BrukerID) REFERENCES Bruker(BrukerID),
 FOREIGN KEY (ServiceID) REFERENCES Service(ServiceID)
 );
+
+INSERT INTO Bruker (Rolle, Brukernavn, Passord) VALUES ("Admin", "ABC", "AQAAAAIAAYagAAAAEOLYh+7jbiqWsyi8baPDB8K04lJDBvqsn3YFWhoOToNJuZufB0C/oVf7jNiDhsIGlw==");
